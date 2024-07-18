fd5a571c655c1a0c2a77a7fa3c94acbbef791d57
Facelles <122049357+Facelles@users.noreply.github.com>
2024-07-18 21:53:15 +0300

    Base

---
+++ src/index.js
@@ -0,0 +1,37 @@
+alert('Hello World!');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let data = [12, 54, 4356.5, "World", 's', false]   //new Array(12, 324, 43);
+//
+// data[0] = "SOS";
+//
+// for (let i = 0; i < data.length ; i++) {
+//     console.log(data[i])
+// }
+// 
+//  let matrix = [[123,34,123], ["World", "Syka", "Vikas"]]
+//
+// for (let i = 0; i < 3; i++) {
+//     console.log(matrix[1 ][i]);
+// }
