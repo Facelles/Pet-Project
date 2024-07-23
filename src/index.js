729d459ae1e1827ec65c557f35b0255352b12483
Facelles <122049357+Facelles@users.noreply.github.com>
2024-07-23 19:56:37 +0300

    "Learned button and function"

--- src/index.js
+++ src/index.js
@@ -1,38 +1,7 @@
-var data = confirm('All is good? yeah? fucking kid???');
-if(data){
-    alert('I love tea! And i stole your tea muheheheh! ');
-}else{
-    console.log("Lalalalalalalalallalalalalallalalalalallal your location 423,456.98,11"); // this joke )=
+function btnclick(){
+    alert('clicked');
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let data = [12, 54, 4356.5, "World", 's', false]   //new Array(12, 324, 43);
-//
-// data[0] = "SOS";
-//
-// for (let i = 0; i < data.length ; i++) {
-//     console.log(data[i])
-// }
-// 
-//  let matrix = [[123,34,123], ["World", "Syka", "Vikas"]]
-//
-// for (let i = 0; i < 3; i++) {
-//     console.log(matrix[1 ][i]);
-// }
+function userInput(el){
+    if(el.value == "font") el.style.fontSize = "10px";
+}
