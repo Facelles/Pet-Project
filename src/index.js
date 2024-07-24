9e9878af00dbe71a9af7705c4322f362419f196d
Facelles <122049357+Facelles@users.noreply.github.com>
2024-07-24 18:19:24 +0300

    "Timer and intervals"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,33 @@
-function btnclick(){
-    alert('clicked');
+// Timer and intervals
+
+var id = setInterval(my_func, 1000);
+
+var TimerId= setTimeout(function (){
+    alert('Message');
+}, 8000);
+
+
+var counter = 0;
+function my_func(){
+    document.getElementById("timer").innerHTML = "Counter: " + counter;
+    counter++;
 }
 
-function userInput(el){
-    if(el.value == "font") el.style.fontSize = "10px";
+function stopInterval(){
+    // clearInterval(id);
+    clearTimeout(TimerId);
 }
+
+setInterval(function (){
+    console.log('Sec...');
+}, 2000)
+
+
+
+// function btnclick(){
+//     alert('clicked');
+// }
+//
+// function userInput(el){
+//     if(el.value == "font") el.style.fontSize = "10px";
+// }
