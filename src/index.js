fad3cc4aecf2b06aaa40cbe9cdf6fc83597faf74
Facelles <122049357+Facelles@users.noreply.github.com>
2024-07-29 12:00:28 +0300

    "I hate my sister"

--- src/index.js
+++ src/index.js
@@ -1,33 +1,64 @@
-// Timer and intervals
+setInterval(function () {
+    var date = new Date();
+    
+    var time = 'Точнa дата: ' + date.toLocaleDateString();
+    
+    document.getElementById('time').innerText = time;
+    
+},1000);
 
-var id = setInterval(my_func, 1000);
-
-var TimerId= setTimeout(function (){
-    alert('Message');
-}, 8000);
-
-
-var counter = 0;
-function my_func(){
-    document.getElementById("timer").innerHTML = "Counter: " + counter;
-    counter++;
-}
-
-function stopInterval(){
-    // clearInterval(id);
-    clearTimeout(TimerId);
-}
-
-setInterval(function (){
-    console.log('Sec...');
-}, 2000)
+var arr= [9, 2 ,4 , "get", false];
+console.log(arr.sort());
 
 
 
-// function btnclick(){
-//     alert('clicked');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Timer and intervals
+//
+// var id = setInterval(my_func, 1000);
+//
+// var TimerId= setTimeout(function (){
+//     alert('Message');
+// }, 8000);
+//
+//
+// var counter = 0;
+// function my_func(){
+//     document.getElementById("timer").innerHTML = "Counter: " + counter;
+//     counter++;
 // }
 //
-// function userInput(el){
-//     if(el.value == "font") el.style.fontSize = "10px";
+// function stopInterval(){
+//     // clearInterval(id);
+//     clearTimeout(TimerId);
 // }
+//
+// setInterval(function (){
+//     console.log('Sec...');
+// }, 2000)
+//
+//
+//
+// // function btnclick(){
+// //     alert('clicked');
+// // }
+// //
+// // function userInput(el){
+// //     if(el.value == "font") el.style.fontSize = "10px";
+// // }
